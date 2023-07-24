9b019827993c805770f4a08e7d90fda538389e6a
PowerUserE <125454293+PowerUserE@users.noreply.github.com>
2023-07-24 21:17:34 +0100

    modified the annotation of the test class.

--- src/test/java/com/example/demo/SpotifyAppTest.java
+++ src/test/java/com/example/demo/SpotifyAppTest.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.spotifyApp.SpotifyApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(classes = SpotifyApp.class) // Replace SpotifyApp with your main application class
+
 class SpotifyAppTest {
 
 	@Test
